8835a2b26918f237db7c60d9a8732ec8a169275a
jcepedaf <jcepedaf@gmail.com>
2021-07-29 22:46:20 +0200

    agrego parte 1 del segundo ejercicio

--- week-1/Homework/dom-manipulation-1/exercise.js
+++ week-1/Homework/dom-manipulation-1/exercise.js
@@ -35,6 +35,12 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
+let buttonAlert = document.querySelector("#alertBtn");
+buttonAlert.addEventListener("click", thanksMessage);
+function thanksMessage(){
+    alert("Thanks for visiting Bikes for Refugees!");
+}
+
 
 /*
 Task 3
