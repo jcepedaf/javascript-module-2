1bb6fbf7600f931b8dbfc508f86dcabe710caff6
jcepedaf <jcepedaf@gmail.com>
2021-08-05 23:25:38 +0200

    agrego ejercicio 1 de A de part-2 de homework

--- week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
@@ -17,3 +17,44 @@ var car = {
 
 */
 
+var kitchen = {
+    brand: "teka",
+    type: "gas",
+    color: "gray",
+    height: "60cm"
+};
+
+var house = {
+    type: "piso",
+    rooms: 4,
+    bathroom: 1,
+    doors: 5,
+    windows: 6,
+    color: "white",
+    size: "70mtr",
+    hasCeramicFloor: false
+};
+
+var cityBarcelona = {
+    hasBeach: true,
+    isBig: true,
+    isMultiCultural: true,
+    population: "5mill",
+    hasMontain: true
+}
+
+var monitor = {
+    size: "24inch",
+    brand: "samsung",
+    isHD: true,
+    color: "gray",
+    isTouchScreen: false
+}
+
+
+var mobilePhone = {
+    brand: "xiaomi",
+    size: "9inch"
+    SO: "android",
+    version: 12
+    }
