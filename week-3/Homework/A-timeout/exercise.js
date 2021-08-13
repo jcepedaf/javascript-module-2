49d9698f39627e9e0c263709118d7a3b306ec814
jcepedaf <jcepedaf@gmail.com>
2021-08-13 22:40:24 +0200

    agrego tarea A de Homework de week 3 de javascript

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -9,3 +9,18 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+//tarea uno
+//setTimeout(function changeBackground() {
+    //document.body.style.backgroundColor = "blue"; },5000); 
+
+//tarea dos
+
+let i = 0; 
+function backgroundChange() {
+    let colors = ["blue", "red", "green", "gray", "brown", "yellow"]; 
+    document.body.style.backgroundColor = colors[i]; 
+    i++ % colors.length;
+};
+
+setInterval(backgroundChange, 5000);
