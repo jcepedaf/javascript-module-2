e06791c6cd1b777418354562a6228838be6ca72f
jcepedaf <jcepedaf@gmail.com>
2021-08-14 00:02:52 +0200

    agrego tarea c de Homework de week 3 de javascript

--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,4 +1,35 @@
-function setAlarm() {}
+let interval;
+
+function setAlarm() { 
+  
+  let alarmSet = document.getElementById("alarmSet").value;
+  let timeRemaining = document.getElementById("timeRemaining");
+  let body = document.querySelector("body");
+
+  
+  clearInterval(interval); 
+  pauseAlarm(); 
+
+  
+  interval = setInterval(()=>{ 
+    if(alarmSet===0){ 
+      timeRemaining.innerText = "Time Remaining: 00:0" + alarmSet;
+      body.style.background = "gray"; 
+      playAlarm(); 
+      clearInterval(interval); 
+    }else if(alarmSet>10){
+      timeRemaining.innerText = "Time Remaining: 00:" + alarmSet;
+      alarmSet --;
+    }else if(alarmSet>9){
+      alarmSet = 9;
+      timeRemaining.innerText = "Time Remaining: 00:0" + alarmSet;
+      alarmSet --;
+    }else{
+      timeRemaining.innerText = "Time Remaining: 00:0" + alarmSet;
+      alarmSet --;
+    };
+  },1000); 
+};
 
 // DO NOT EDIT BELOW HERE
 
